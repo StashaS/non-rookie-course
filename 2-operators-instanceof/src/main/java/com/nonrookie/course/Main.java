b6d1c62b790a2a548fa89279d7322d75afcfba9e
Anastasiia Sarycheva <st.sarychev@gmail.com>
2021-06-08 07:06:28 +0100

    2nd meeting example - operators, instanceof

---
+++ 2-operators-instanceof/src/main/java/com/nonrookie/course/Main.java
@@ -0,0 +1,13 @@
+package com.nonrookie.course;
+
+import com.nonrookie.course.instance_of.DemoInstanceof;
+import com.nonrookie.course.operator.DemoOperator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DemoOperator.demonstration();
+        DemoInstanceof.demonstration();
+    }
+
+}
